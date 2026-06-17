bd412261277762cd75da7dbbe54cbbc9fa134944
Leoanrdo Mosquera <mmosquera@cuentadealtocosto.org>
2026-06-17 11:26:08 -0500

    Infra: configurar CSRF para admin test

--- cacplay-backend/config/settings.py
+++ cacplay-backend/config/settings.py
@@ -31,6 +31,8 @@ DEBUG = env_bool('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = env_list('DJANGO_ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
 
+CSRF_TRUSTED_ORIGINS = env_list('DJANGO_CSRF_TRUSTED_ORIGINS', [])
+
 # Application definition
 
 INSTALLED_APPS = [
