66f818f5f4829d04dd6d6c3a967c3ec406937aa8
JoseLubo <tjlubo_51@hotmail.com>
2026-06-19 10:04:57 -0500

    feat: se vuelve a iniciar el cargue de los archivos ajustados sin el environment

--- cacplay-frontend/src/environments/environment.ts
+++ cacplay-frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-<<<<<<< HEAD
+
   production: true,
   apiUrl: 'https://cacplaytest.cuentadealtocosto.org/api'
 =======
   production: false,
   apiUrl: '/api'
->>>>>>> 4a1a737141d61515a08a75fb2a2416abf6fcddef
+
 };
