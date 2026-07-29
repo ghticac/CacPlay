482598107105dddb6e6960e8292df6f422594437
JoseLubo <tjlubo_51@hotmail.com>
2026-07-29 12:39:37 -0500

    icono de pantalla completa

--- cacplay-frontend/src/app/layout/main-layout/main-layout.ts
+++ cacplay-frontend/src/app/layout/main-layout/main-layout.ts
@@ -40,4 +40,40 @@ export class MainLayout {
     this.authService.logout();
     window.location.href = '/intro';
   }
+
+
+  // Agregamos el estado para cambiar el ícono (expandir / comprimir)
+esPantallaCompleta: boolean = false;
+
+toggleFullScreen() {
+  const doc = document as any;
+  const docEl = document.documentElement as any;
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    // Entrar a pantalla completa
+    if (docEl.requestFullscreen) {
+      docEl.requestFullscreen();
+    } else if (docEl.msRequestFullscreen) {
+      docEl.msRequestFullscreen();
+    } else if (docEl.mozRequestFullScreen) {
+      docEl.mozRequestFullScreen();
+    } else if (docEl.webkitRequestFullscreen) {
+      docEl.webkitRequestFullscreen();
+    }
+    this.esPantallaCompleta = true;
+  } else {
+    // Salir de pantalla completa
+    if (doc.exitFullscreen) {
+      doc.exitFullscreen();
+    } else if (doc.msExitFullscreen) {
+      doc.msExitFullscreen();
+    } else if (doc.mozCancelFullScreen) {
+      doc.mozCancelFullScreen();
+    } else if (doc.webkitExitFullscreen) {
+      doc.webkitExitFullscreen();
+    }
+    this.esPantallaCompleta = false;
+  }
+}
+
 }
